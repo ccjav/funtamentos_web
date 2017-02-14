63ab4d3d341137c39b492171ce0ec7c1846acab3
jja <gitter@tutanota.com>
2017-02-13 19:33:38 -0600

    opacidad en navegación, detalles de diseño navegación

--- martes/fixed_navigation/navbar.js
+++ martes/fixed_navigation/navbar.js
@@ -1,4 +1,8 @@
 $(window).on("scroll", function () {
-  // Aquí deberás escribir la lógica que modificará la barra
-  
+  	// Aquí deberás escribir la lógica que modificará la barra
+	if ($(document).scrollTop() > 65) {
+		$(".top-nav").addClass("opacize");
+	} else {
+		$(".top-nav").removeClass("opacize");
+	}
 });
