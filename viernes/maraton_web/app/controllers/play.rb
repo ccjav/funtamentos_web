b4bf1ca1aaeb69535d837d8a0dd107925a5320d9
jja <gitter@tutanota.com>
2017-02-22 20:59:33 -0600

    maraton; lógica de rondas

--- viernes/maraton_web/app/controllers/play.rb
+++ viernes/maraton_web/app/controllers/play.rb
@@ -3,46 +3,58 @@ get '/play' do
   erb :play
 end
 
-get '/decks/:deck_id' do 
+get '/decks/:deck_id' do
   card = Card.where(deck_id: params[:deck_id])
+  p "CURRENT CARD!! #{card[0].id}"
   session[:card] = 0
   @card = card[session[:card]]
+  puts "SESSION EN GET"
+  p session[:card]
+  p @card.id
   # @deck = Deck.find(params[:deck_id])
   # if @deck.cards[0].id == @card.id
   #   round = Round.create(deck_id: params[:deck_id])
-  #   session[:round] = round.id
   # end
+  # round = Round.create(deck_id: 1)
+  #   session[:round] = round.id
+
   erb :deck
 end
 
 post '/play' do
   # card = Card.where(deck_id: params[:deck_id])
+  p session
   round = Round.where(id: session[:round])
+  p round
+
   deck = Deck.find(round[0].deck_id)
   p "DECK #{deck.cards[0]}"
   p round[0].deck_id
   # p round[0].guesses
-  session[:card] += 1
-  puts "PARAMS #{params}"
-  p session
+  p "PARAMS #{params}"
   @card = deck.cards[session[:card]]
-  # if params[:answer] == @card.answer
-  #   Guess.create(correct: true, round_id: session[:round])
-  #   @right_wrong = "Bien"
-  #   # <p><a href=\"/decks/#{params[:deck_id]}/cards/#{params[:card_id].to_i + 1}\">Siguiente</a></p>"
-  #   # redirect "/decks/#{params[:deck_id]}/cards/#{params[:card_id].to_i + 1}"
-  # else
-  #   Guess.create(correct: false, round_id: session[:round])
-  #   @right_wrong = "Mal"
+  p @card.answer
+  p params[:answer]
+  if params[:answer] == @card.answer
+    Guess.create(correct: true, round_id: session[:round])
+    @right_wrong = "Bien"
+    # <p><a href=\"/decks/#{params[:deck_id]}/cards/#{params[:card_id].to_i + 1}\">Siguiente</a></p>"
+    # redirect "/decks/#{params[:deck_id]}/cards/#{params[:card_id].to_i + 1}"
+  else
+    Guess.create(correct: false, round_id: session[:round])
+    @right_wrong = "Mal"
 
-  #       # redirect "/decks/#{params[:deck_id]}/cards/#{params[:card_id].to_i + 1}"
+        # redirect "/decks/#{params[:deck_id]}/cards/#{params[:card_id].to_i + 1}"
 
-  # end
+  end
   p "CARD #{@card.id}"
-  # if deck.cards[-1].id == @card.id
-  #   @hola = "terminaste"
-  # end
+  if deck.cards[-1].id == @card.id
+    @hola = "terminaste"
+  end
+  session[:card] += 1
   erb :_deck_form, layout: false
+
+
   # redirect "/decks/#{params[:deck_id]}/#{params[:card_id].to_i + 1}"
 
 end
--- viernes/maraton_web/public/js/application.js
+++ viernes/maraton_web/public/js/application.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  $( "#answer_form" ).on( "submit", function( event ) {
+  $( "#rightWrong" ).on( "submit", function( event ) {
     event.preventDefault();
-    // console.log( $(this).attr('data-deck') );
-    var card = $(this).attr('data-card')
-    var formData = $( this ).serialize() + "&card=" + card;
+    // console.log( $(this).attr('data-card') + "hola2");
+    console.log( $(this).find("#answer_form").attr('data-card') );
+    var card = $(this).find("#answer_form").attr('data-card')
+    var formData = $( this ).find("#answer_form").serialize() + "&card=" + card;
     $.post( "/play", formData, function( data ) {
       console.log( data )
       $( "#rightWrong" ).html( data );
